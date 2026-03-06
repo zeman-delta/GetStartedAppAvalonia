151e107881755e8e384269ea724b77977b95d795
Martin Zeman <martin.zeman@freelo.io>
2026-03-06 11:11:12 +0100

    zobrazení pole

--- GetStartedApp/Models/Person.cs
+++ GetStartedApp/Models/Person.cs
@@ -1,46 +1,24 @@
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace GetStartedApp.Models;
 
-public class Person : INotifyPropertyChanged
+public class Person: ObservableObject
 {
-
+    
     private string _name;
+    
     private int _age;
-
-    public Person(string name, int age)
-    {
-        Name = name;
-        Age = age;
-    }
 
     public string Name
     {
         get { return _name; }
-        set
-        {
-            _name = value;
-            OnPropertyChanged(nameof(Name));
-        }
+        set { SetProperty(ref _name, value); }
     }
 
     public int Age
     {
         get { return _age; }
-        set
-        {
-            _age = value;
-            OnPropertyChanged(nameof(Age));
-        }
+        set { SetProperty(ref _age, value); }
     }
-
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+    
 }
--- GetStartedApp/ViewModels/MainWindowViewModel.cs
+++ GetStartedApp/ViewModels/MainWindowViewModel.cs
@@ -1,30 +1,25 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
+﻿using System.Collections.ObjectModel;
+using GetStartedApp.Models;
 
 namespace GetStartedApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
-
-    [ObservableProperty]
-    private string _celsius = "0";
-
-    [ObservableProperty]
-    private string _fahrenheit = "0";
-
-    [RelayCommand]
-    private void Convert()
+    
+    private ObservableCollection<Person> _people;
+    public ObservableCollection<Person> People
     {
-        if (double.TryParse(Celsius, out double c))
-        {
-            var f = c * (9d / 5d) + 32;
-            Fahrenheit = f.ToString("0.0");
-        }
-        else
-        {
-            Celsius = "0";
-            Fahrenheit = "0";
-        }
+        get => _people;
     }
+
+    public MainWindowViewModel()
+    {
+        _people = new ObservableCollection<Person>()
+        {
+            new Person { Name = "Jan", Age = 30 },
+            new Person { Name = "Honza", Age = 28 },
+            new Person { Name = "Michal", Age = 45 }
+        };
+    }
+
 }
